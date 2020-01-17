67f3d57a365c273ac187656b2bb5ee0794d121d3
Spencer <spencer.jackson@iconfitness.com>
2020-01-17 15:43:09 -0700

    scrapes all the pages now

--- ksl_auto_analyzer.py
+++ ksl_auto_analyzer.py
@@ -5,34 +5,55 @@ import urllib.request
 import re
 import copy
 import numpy
-import matplotlib
+import matplotlib.pyplot
 import sys
+import time
 
-#url = sys.argv[1]+"&page="
-url = "https://cars.ksl.com/search/index?make[]=Nissan&model[]=Leaf"
-#page = urllib.request.urlopen(url+"0")
+make = "Toyota"
+model = "Corolla"
+url = "https://cars.ksl.com/search/index?make[]="+make+"&model[]="+model+"&page="
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
-req = urllib.request.Request(url,None,hdr)
-page = urllib.request.urlopen(req)
 
-#print(page.read())
-data = page.read().decode('utf-8')
-prices = [int(m.group()) for m in re.finditer('(?<=class="listing-detail-line price" data-price=")(\d+)(?=")',data)]
-miles = [int(m.group().replace(",","")) for m in re.finditer('(?<=Mileage: )\d{1,3}(,\d{3})*',data)]
+prevp = []
+prevm = []
+prices = []
+miles = []
+duplicate = False
+pagen = 0
+start = time.time()
+while not duplicate:
+    req = urllib.request.Request(url+str(pagen),None,hdr)
+    page = urllib.request.urlopen(req)
+    pagen += 1
 
-print(miles,prices)
-exit()
-
-top1000 = top100
-
-for p in range(2,11):
-    url = "https://boardgamegeek.com/browse/boardgame/page/"+str(p)
-    page = urllib.request.urlopen(url)
     data = page.read().decode('utf-8')
-    top = [int(m.group()) for m in re.finditer('(?<=href="/boardgame/)(\d+)(?=/)',data)]
-    top1000.extend(top[0::3])
+
+    p = [float(m.group()) for m in re.finditer('(?<=class="listing-detail-line price" data-price=")(\d+)(\.\d{2})?(?=")',data)]
+    m = [int(m.group().replace(",","")) for m in re.finditer('(?<=Mileage: )\d{1,3}(,\d{3})*   ',data)]
+    if(len(p) != len(m)):
+        print("error,",len(m),"vs",len(p),"listings found on page",pagen+1)
+        #print(data)
+        print(m)
+        print(p)
+        exit()
+    if(p != prevp and m != prevm):
+        prevp = p
+        prevm = m
+        prices += p
+        miles += m
+        print(len(miles),"listings found in",time.time()-start,"seconds")
+    else:
+        duplicate = True
+        
+#print(miles)
+#print(prices)
+matplotlib.pyplot.scatter(miles,prices)
+matplotlib.pyplot.ylabel('price')
+matplotlib.pyplot.xlabel('mileage')
+matplotlib.pyplot.show()
+exit()
